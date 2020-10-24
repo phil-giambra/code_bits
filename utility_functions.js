1953f2aa5800775571b76215e54a7f1be967c2ad
phil-giambra <philgiambra@gmail.com>
2020-10-24 13:40:58 -0400

    added utilities

---
+++ utility_functions.js
@@ -0,0 +1,71 @@
+
+
+// returns a string such as "2020-10-13" for the given seconds since epoch or the current day if secs is undefined
+function getDateNow(secs) {
+    let d
+    if (secs === undefined) { d = new Date() } else { d = new Date(secs) }
+    let datenow  = [  d.getFullYear(),  ('0' + (d.getMonth() + 1)).slice(-2),  ('0' + d.getDate()).slice(-2)].join('-');
+    return datenow
+}
+
+// returns a string such as "14:25" for the given seconds since epoch or the current time if secs is undefined
+function getTimeNow(secs) {
+    let d
+    if (secs === undefined) { d = new Date() } else { d = new Date(secs) }
+    let datenow  = [    ('0' + d.getHours() ).slice(-2),  ('0' + d.getMinutes()).slice(-2)].join(':');
+    return datenow
+}
+
+// returns seconds since epoch for the given date and time
+function getSecondsForDatetime( date, time ) {  // ex getSecondsForDatetime("2019-07-07", "04:15")
+    let timestring = date+"T"+time
+    let seconds = new Date(timestring).getTime();
+    return seconds
+}
+
+
+
+// a few diffenernt uuid creation functions
+
+function generateUUID() {
+    let d = new Date().getTime();
+    if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+        d += performance.now(); //use high-precision timer if available
+    }
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        let r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
+
+// http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/21963136#21963136
+function generateUUID2 () {
+    var _lut = [];
+	for ( var i = 0; i < 256; i ++ ) {
+		_lut[ i ] = ( i < 16 ? '0' : '' ) + ( i ).toString( 16 );
+	}
+
+    var d0 = Math.random() * 0xffffffff | 0;
+    var d1 = Math.random() * 0xffffffff | 0;
+    var d2 = Math.random() * 0xffffffff | 0;
+    var d3 = Math.random() * 0xffffffff | 0;
+    var uuid = _lut[ d0 & 0xff ] + _lut[ d0 >> 8 & 0xff ] + _lut[ d0 >> 16 & 0xff ] + _lut[ d0 >> 24 & 0xff ] + '-' +
+        _lut[ d1 & 0xff ] + _lut[ d1 >> 8 & 0xff ] + '-' + _lut[ d1 >> 16 & 0x0f | 0x40 ] + _lut[ d1 >> 24 & 0xff ] + '-' +
+        _lut[ d2 & 0x3f | 0x80 ] + _lut[ d2 >> 8 & 0xff ] + '-' + _lut[ d2 >> 16 & 0xff ] + _lut[ d2 >> 24 & 0xff ] +
+        _lut[ d3 & 0xff ] + _lut[ d3 >> 8 & 0xff ] + _lut[ d3 >> 16 & 0xff ] + _lut[ d3 >> 24 & 0xff ];
+
+    // .toUpperCase() here flattens concatenated strings to save heap memory space.
+    return uuid.toUpperCase();
+}
+
+
+
+// browser / electron only
+
+function uuidv4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
